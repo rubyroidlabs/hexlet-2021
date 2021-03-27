20356b32329d245ccdc049fe4bfd12b93bc9615c
Anna Velentsevich <annvelents@gmail.com>
2021-03-27 08:06:07 +0300

    implement first homework

---
+++ Velentsevich_Anna/1/first.rb
@@ -0,0 +1,12 @@
+puts 'I\'m a bad file'
+
+a=b=3
+
+if a>=1 and b >=2
+   a = a + 1
+   b = b - 1
+end
+
+puts "long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long-long string"
+
+puts a+b
