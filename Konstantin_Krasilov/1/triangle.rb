a23491b662b494de5723190b2dc604d536bbf4b2
kkrasilov <kkrasilov@gmail.com>
2021-03-30 19:39:24 +0500

    triangle project

--- Konstantin_Krasilov/1/triangle.rb
+++ Konstantin_Krasilov/1/triangle.rb
@@ -14,7 +14,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  return :equilateral if a == b && a == c
+  return :isosceles if a == b || b == c || c == a
+
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
