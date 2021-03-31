d33f46f1f4dc15d28dd1822befec3742239e7056
kkrasilov <kkrasilov@gmail.com>
2021-03-31 12:52:45 +0500

    triangle

--- Konstantin_Krasilov/1/triangle.rb
+++ Konstantin_Krasilov/1/triangle.rb
@@ -12,8 +12,15 @@
 #   about_triangle_project.rb
 # and
 #   about_triangle_project_2.rb
-#
+require 'pry'
 def triangle(a, b, c)
+
+  raise TriangleError, 'triangle should not have any sides of length 0' if [a, b, c].sum.zero?
+  raise TriangleError, "negative length doesn't make sense." if [a, b, c].min.negative?
+
+  x, y, z = [a, b, c].sort
+  raise TriangleError, 'any two sides of a triangle should add up to more than the third side' if x + y <= z
+
   return :equilateral if a == b && a == c
   return :isosceles if a == b || b == c || c == a
 
