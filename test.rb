2b28384755614e42e0d7ea56b790e94025a6e71c
Anna Velentsevich <annvelents@gmail.com>
2021-03-31 23:07:14 +0300

    More DESTRUPTION

--- test.rb
+++ test.rb
@@ -1,10 +1,10 @@
 
 
-a = 1
-b = 2
-if a and b
-  a = a + 1
-  b = b - 1
-end
+ a = 1
+ b = 2
+ if a and b
+   a = a + 1
+   b = b - 1
+ end
 
-puts 'a + b = ' + (a + b).to_s
+ puts 'a + b = ' + (a + b).to_s
