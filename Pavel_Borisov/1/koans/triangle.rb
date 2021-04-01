5c3814b275b64cec2d530d4945b8f3f60f27cd69
ashpb <31402193+ashpb@users.noreply.github.com>
2021-04-01 20:21:43 +1000

    Complete triangle

--- Pavel_Borisov/1/koans/triangle.rb
+++ Pavel_Borisov/1/koans/triangle.rb
@@ -14,7 +14,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  return :equilateral if a == b && b == c
+  return :isosceles if a == b || a == c || b == c
+
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
