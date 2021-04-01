e15ea32b8cafd34f7836e449065214cc4023c14e
kkrasilov <kkrasilov@gmail.com>
2021-04-01 15:22:47 +0500

    proxy object project

--- Konstantin_Krasilov/1/about_proxy_object_project.rb
+++ Konstantin_Krasilov/1/about_proxy_object_project.rb
@@ -13,9 +13,24 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader :messages
+
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = []
+  end
+
+  def called?(method)
+    @messages.include?(method)
+  end
+
+  def number_of_times_called(method)
+    @messages.count(method)
+  end
+
+  def method_missing(method_name, *args, &block)
+    @messages << method_name
+    @object.send(method_name, *args, &block)
   end
 
   # WRITE CODE HERE
