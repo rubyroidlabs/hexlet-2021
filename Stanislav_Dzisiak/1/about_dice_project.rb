1322016c5f96c8767dab130de7f1e7a7572ee5b0
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-04-04 14:30:13 +0300

    solve dice_project

--- Stanislav_Dzisiak/1/about_dice_project.rb
+++ Stanislav_Dzisiak/1/about_dice_project.rb
@@ -2,9 +2,13 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_reader :values
+
+  def roll(value)
+    @values = (1..value).to_a.shuffle
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
