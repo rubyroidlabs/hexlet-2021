3723b6e7df1d77cd729a74ed72531e035c4e6a3f
ArtemShadovskiy <>
2021-04-07 13:10:33 +0000

    complete about_proxy_object

--- Artyom_Peredery/1/koans/about_proxy_object_project.rb
+++ Artyom_Peredery/1/koans/about_proxy_object_project.rb
@@ -13,11 +13,25 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_accessor :messages
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = []
   end
-
+  def number_of_times_called(method_name)
+    @messages.count(method_name)
+  end
+  def called?(method_name) 
+    @messages.include?(method_name)
+  end
+  def method_missing(method_name, *args, &block)
+    if @object.respond_to?(method_name)
+      @messages.push(method_name)
+      @object.send(method_name, *args)
+    else
+      super(method_name, *args, &block)
+    end
+  end
   # WRITE CODE HERE
 end
 
