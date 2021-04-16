dfb051d99cd25536931ff8d7ca299343e74d88fb
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-04-16 23:39:41 +0300

    add rubocop & fix lint

--- Stanislav_Dzisiak/2/lib/url_analyzer.rb
+++ Stanislav_Dzisiak/2/lib/url_analyzer.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "url_analyzer/version"
-
 module UrlAnalyzer
   class Error < StandardError; end
   # Your code goes here...
--- Stanislav_Dzisiak/2/spec/url_analyzer_spec.rb
+++ Stanislav_Dzisiak/2/spec/url_analyzer_spec.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-RSpec.describe UrlAnalyzer do
-  it "has a version number" do
-    expect(UrlAnalyzer::VERSION).not_to be nil
-  end
+require_relative '../lib/url_analyzer/version'
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe UrlAnalyzer do
+  it 'has a version number' do
+    expect(UrlAnalyzer::VERSION).not_to be nil
   end
 end
