3aa6c960402affbd0bb3bfd05099dc1b96d69b8f
kkrasilov <kkrasilov@gmail.com>
2021-04-18 20:12:30 +0500

    Added options parser

--- Konstantin_Krasilov/2/checker.rb
+++ Konstantin_Krasilov/2/checker.rb
@@ -2,3 +2,15 @@
 # frozen_string_literal: true
 
 # Checker links
+
+require_relative 'lib/options_parser'
+
+options = OptionsParser.new.call
+
+if options.key?(:error)
+  puts "Sorry, an error has occurred - #{options[:error]}\n #{options[:help]}"
+  exit
+elsif ARGV.first.nil? || !ARGV.first.match?(/.csv$/)
+  puts 'Please select the correct CSV file.'
+  exit
+end
