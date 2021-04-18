dfc030165c514e8dcf99ac6f34d98b7617463551
kkrasilov <kkrasilov@gmail.com>
2021-04-18 20:14:16 +0500

    Added reader

---
+++ Konstantin_Krasilov/2/lib/reader.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'csv'
+require_relative 'link'
+# Reader
+module Reader
+  module_function
+
+  OPEN_SOURCE = /(gitlab|phabricator|redmine|rhodecode|github|bitbucket|gogs|gitea|sourcehut)/.freeze
+
+  def from_csv(path, options = {})
+    CSV.read(path).filter_map do |url|
+      next if options.key?(:solutions) && url.join.match?(OPEN_SOURCE)
+      next if options.key?(:subdomains) && url.join.count('.') > 1
+
+      Link.new(url.join)
+    end
+  end
+end
