b1473a897e97f29905477fe2579a6a0c3cd4c110
ashpb <31402193+ashpb@users.noreply.github.com>
2021-04-20 12:53:09 +1000

    Implement DomainsList and DomainChecker

--- Pavel_Borisov/2/checker.rb
+++ Pavel_Borisov/2/checker.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 require 'optparse'
-# require 'csv'
+require 'csv'
+require 'net/http'
 
 params = {}
 OptionParser.new do |opts|
@@ -13,6 +16,68 @@ domain_list_filename = ARGV.first
 
 unless !domain_list_filename.nil? && File.exists?(domain_list_filename)
   fail(ArgumentError, "Domain list filename incorrect or not provided")
+end
+
+class DomainsList
+  attr_reader :list
+
+  def initialize(filename, params)
+    @list = CSV.read(filename).map(&:first)
+
+    if params[:'no-subdomains']
+      reject_subdomains!
+    end
+
+    if params[:'exclude-solutions']
+      reject_solutions!
+    end
+  end
+
+  private
+
+  def reject_subdomains!
+    @list.filter!  { |domain| domain.count('.') == 1 }
+  end
+
+  def reject_solutions!
+    @list.filter! do |domain|
+      !domain.match(/(\bgitlab\b|\bredmine\b|\bgit\b|\brepo\b|\brm\b|\bsource\b|\bsvn\b)/)
+    end
+  end
+end
+
+class DomainChecker
+  attr_reader :domain, :code, :response_time, :body, :status
+
+  def initialize(domain)
+    @uri = URI("https://#{domain}/")
+    @domain = domain
+  end
+
+  def check!
+    begin
+      start_time = Time.now
+      response = Net::HTTP.get_response(@uri)
+      end_time = Time.now
+      @response_time = "#{((end_time - start_time) * 1000).round}ms"
+      @code = response.code
+      @body = response.body
+      @status = :success
+    rescue SocketError => e
+      @status = :failure
+      @error_message = "ERROR (#{e.cause})"
+    end
+    self
+  end
+
+  def to_s
+    case @status
+    when :success
+      "#{@domain} - #{@code} (#{@response_time})"
+    when :failure
+      "#{domain} - #{error_message}"
+    end
+  end
 end
 
 
