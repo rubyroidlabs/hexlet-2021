1285ef19690ce2be3c7f13f4f1acd7ff9b95144b
Aleksandrova Vilena <alviro12@gmail.com>
2021-04-22 01:09:56 +0500

    [task-2] review: tests+

--- Aleksandrova_Vilena/2/checker/spec/args_parser_spec.rb
+++ Aleksandrova_Vilena/2/checker/spec/args_parser_spec.rb
@@ -4,9 +4,8 @@ require 'rspec'
 
 require_relative '../lib/helper'
 
-RSpec.describe 'ArgsParser: --subdomains' do
-  # let(:exec) { File.expand_path('../checker.rb', File.dirname(__FILE__)) }
-  subject { ArgsParser.parse(args) }
+RSpec.describe ArgsParser do
+  subject { described_class.parse(args) }
   let(:args) { %w[-n test_out.csv] }
 
   it ':subdomains' do
@@ -14,26 +13,26 @@ RSpec.describe 'ArgsParser: --subdomains' do
   end
 end
 
-RSpec.describe 'ArgsParser: --opensource' do
-  subject { ArgsParser.parse(args) }
+RSpec.describe ArgsParser do
+  subject { described_class.parse(args) }
   let(:args) { %w[-r test_out.csv] }
   it ':opensource' do
     expect(subject).to eq Hash[:opensource, true]
   end
 end
 
-RSpec.describe 'ArgsParser: --parallel' do
-  subject { ArgsParser.parse(args) }
+RSpec.describe ArgsParser do
+  subject { described_class.parse(args) }
   let(:args) { %w[-p=3 test_out.csv] }
   it ':parallel' do
     expect(subject).to eq Hash[:parallel, '=3']
   end
 end
 
-RSpec.describe 'ArgsParser: --filter' do
-  subject { ArgsParser.parse(args) }
+RSpec.describe ArgsParser do
+  subject { described_class.parse(args) }
   let(:args) { %w[-f=KEYWORD test_out.csv] }
-  it ':parallel' do
+  it ':filter KEYWORD' do
     expect(subject).to eq Hash[:filter, '=KEYWORD']
   end
 end
