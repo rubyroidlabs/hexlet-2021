ec151a1408cecf1de14c7931f656ccecf50f8cf3
Aleksandrova Vilena <alviro12@gmail.com>
2021-04-25 00:26:12 +0500

    [task-2] tests

--- Aleksandrova_Vilena/2/checker/spec/ping_spec.rb
+++ Aleksandrova_Vilena/2/checker/spec/ping_spec.rb
@@ -28,61 +28,46 @@ RSpec.describe Ping do
         expect(subject.responses.at(0).code).to eq 200
       end
     end
-  end
-end
 
-RSpec.describe Ping do
-  describe '#run' do
-    let(:file) { File.expand_path('../data/rails_test_os.csv', __dir__) }
-    subject { described_class.new(file, opensource: true) }
     context '--opensource filter' do
+      let(:file) { File.expand_path('../data/rails_test_os.csv', __dir__) }
+      subject { described_class.new(file, opensource: true) }
       before { subject.run }
       it 'empty response' do
         expect(subject.responses.count).to eq 0
       end
     end
-  end
-end
 
-RSpec.describe Ping do
-  describe '#run' do
-    let(:file) { File.expand_path('../data/rails_test_summary.csv', __dir__) }
-    subject { described_class.new(file, parallel: '3') }
     context '--parallel filter' do
+      let(:file) { File.expand_path('../data/rails_test_summary.csv', __dir__) }
+      subject { described_class.new(file, parallel: '3') }
       before { subject.run }
       it 'checks the number of threads' do
         expect(subject.pool_size).to eq 3
       end
     end
-  end
-end
 
-RSpec.describe Ping do
-  describe '#run' do
-    let(:file) { File.expand_path('../data/rails_test_200.csv', __dir__) }
-    subject { described_class.new(file, filter: 'WINNIEPOOH') }
     context '--filter KEYWORD' do
+      let(:file) { File.expand_path('../data/rails_test_200.csv', __dir__) }
+      subject { described_class.new(file, filter: 'WINNIEPOOH') }
       before { subject.run }
       it 'did not find a keyword WINNIEPOOH' do
         expect(subject.responses.count).to eq 0
       end
     end
-  end
-end
 
-RSpec.describe Ping do
-  let(:file) { File.expand_path('../data/rails_test_summary.csv', __dir__) }
-  subject { described_class.new(file, {}) }
-
-  describe '#run' do
-    it 'summary equals' do
-      subject.run
-      expect(subject.responses.count).to eq 4
-      expect(subject.responses.count(&:error?)).not_to eq 0
-      expect(subject.responses.count(&:success?)).to eq 2
-      expect(subject.responses.count(&:fail?)).to eq 1
-      expect(subject.responses.select(&:error?).at(0).time).to eq nil
-      expect(subject.responses.select(&:error?).at(0).msg).not_to eq nil
+    context 'summary equals' do
+      let(:file) { File.expand_path('../data/rails_test_summary.csv', __dir__) }
+      subject { described_class.new(file, {}) }
+      before { subject.run }
+      it 'summary equals' do
+        expect(subject.responses.count).to eq 4
+        expect(subject.responses.count(&:error?)).not_to eq 0
+        expect(subject.responses.count(&:success?)).to eq 2
+        expect(subject.responses.count(&:fail?)).to eq 1
+        expect(subject.responses.select(&:error?).at(0).time).to eq nil
+        expect(subject.responses.select(&:error?).at(0).msg).not_to eq nil
+      end
     end
   end
 end
