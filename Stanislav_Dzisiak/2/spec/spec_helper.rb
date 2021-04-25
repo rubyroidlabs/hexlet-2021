2a10dbaf33b622bdf02f3abdfb1ae60c857f5531
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-04-25 15:37:32 +0300

    improve tests

--- Stanislav_Dzisiak/2/spec/spec_helper.rb
+++ Stanislav_Dzisiak/2/spec/spec_helper.rb
@@ -5,10 +5,10 @@ require 'vcr'
 require 'webmock/rspec'
 
 def get_fixture_path(fixture_name)
-  File.join __dir__, 'fixtures', fixture_name
+  File.join(__dir__, 'fixtures', fixture_name)
 end
 
 VCR.configure do |c|
   c.cassette_library_dir = 'vcr_cassettes'
-  c.hook_into :webmock
+  c.hook_into(:webmock)
 end
--- Stanislav_Dzisiak/2/spec/url_analyzer/analyzer_analyze_spec.rb
+++ Stanislav_Dzisiak/2/spec/url_analyzer/analyzer_analyze_spec.rb
@@ -2,42 +2,47 @@
 
 require_relative '../spec_helper'
 
-def analyze_with(options = {})
-  path_to_csv = get_fixture_path 'example.csv'
-  analyzer = UrlAnalyzer::Analyzer.new path_to_csv, options
-  analyzer.analyze
-end
-
 # rubocop:disable Metrics/BlockLength
 describe UrlAnalyzer::Analyzer, '#analyze' do
+  subject(:analyze) do
+    path_to_csv = get_fixture_path('example.csv')
+    analyzer = described_class.new(path_to_csv, options)
+    analyzer.analyze
+  end
+
+  let(:options) { {} }
+
   describe 'requests without body' do
     before { VCR.insert_cassette('example_head') }
 
     after { VCR.eject_cassette }
 
-    it 'default' do
-      data = analyze_with
-      expect(data).to eq({ total: 30, success: 22, failed: 3, errored: 5 })
+    context 'with default options' do
+      it { expect(analyze).to eq({ total: 30, success: 22, failed: 3, errored: 5 }) }
     end
 
-    it 'default with parallel' do
-      data = analyze_with parallel: 10
-      expect(data).to eq({ total: 30, success: 22, failed: 3, errored: 5 })
+    context 'with parallel' do
+      let(:options) { { parallel: 10 } }
+
+      it { expect(analyze).to eq({ total: 30, success: 22, failed: 3, errored: 5 }) }
     end
 
-    it 'no subdomains' do
-      data = analyze_with no_subdomains: true
-      expect(data).to eq({ total: 17, success: 13, failed: 1, errored: 3 })
+    context 'with no subdomains' do
+      let(:options) { { no_subdomains: true } }
+
+      it { expect(analyze).to eq({ total: 17, success: 13, failed: 1, errored: 3 }) }
     end
 
-    it 'exclude solutions' do
-      data = analyze_with exclude_solutions: true
-      expect(data).to eq({ total: 21, success: 14, failed: 2, errored: 5 })
+    context 'with exclude solutions' do
+      let(:options) { { exclude_solutions: true } }
+
+      it { expect(analyze).to eq({ total: 21, success: 14, failed: 2, errored: 5 }) }
     end
 
-    it 'exclude solutions & no subdomains' do
-      data = analyze_with exclude_solutions: true, no_subdomains: true
-      expect(data).to eq({ total: 12, success: 8, failed: 1, errored: 3 })
+    context 'with exclude solutions & no subdomains' do
+      let(:options) { { exclude_solutions: true, no_subdomains: true } }
+
+      it { expect(analyze).to eq({ total: 12, success: 8, failed: 1, errored: 3 }) }
     end
   end
 
@@ -46,14 +51,16 @@ describe UrlAnalyzer::Analyzer, '#analyze' do
 
     after { VCR.eject_cassette }
 
-    it 'filter' do
-      data = analyze_with filter: 'copy'
-      expect(data).to eq({ total: 4, success: 4, failed: 0, errored: 0 })
+    context 'with filter' do
+      let(:options) { { filter: 'copy' } }
+
+      it { expect(analyze).to eq({ total: 4, success: 4, failed: 0, errored: 0 }) }
     end
 
-    it 'filter & no subdomains' do
-      data = analyze_with filter: 'copy', no_subdomains: true
-      expect(data).to eq({ total: 2, success: 2, failed: 0, errored: 0 })
+    context 'with filter & no subdomains' do
+      let(:options) { { filter: 'copy', no_subdomains: true } }
+
+      it { expect(analyze).to eq({ total: 2, success: 2, failed: 0, errored: 0 }) }
     end
   end
 end
