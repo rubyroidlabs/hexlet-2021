96f3c8d691efe3c74ff1a8a403d9151c1b9d01d3
yorickov <yorickov@gmail.com>
2021-05-02 19:32:07 +0300

    refactor tests

--- Dzmitry-Monich/3/fasteng/spec/fasteng/dispatcher_spec.rb
+++ Dzmitry-Monich/3/fasteng/spec/fasteng/dispatcher_spec.rb
@@ -11,83 +11,97 @@ describe Fasteng::Dispatcher do
     let(:message) { double('message', text: '/start', from: from, chat: chat) }
     let(:welcome_message) { Fasteng::MessageSender::ReplyMessage::MESSAGES[:welcome] }
 
-    before do
-      allow(bot_api)
+    it 'user registers' do
+      allow(Fasteng::MessageSender::ReplyMessage)
+        .to receive(:send)
+        .with(bot_api, chat.id, :welcome)
+
+      expect { described_class.call(bot_api, message) }.to change(User, :count).from(0).to(1)
+      expect(User.first.status).to eq 'registered'
+    end
+
+    it 'user gets Welcome message' do
+      expect(bot_api)
         .to receive(:send_message)
         .with(chat_id: chat.id, text: welcome_message)
-    end
 
-    it 'user added in database' do
-      expect { described_class.call(bot_api, message) }.to change(User, :count).from(0).to(1)
-    end
-
-    it 'user gets status registered' do
       described_class.call(bot_api, message)
-
-      expect(User.first.status).to eq 'registered'
     end
   end
 
-  describe 'User gets schedule' do
+  describe 'User gets schedule of getting definitions' do
     context 'when answer valid' do
       let(:message) { double('message', text: '5', from: from, chat: chat) }
       let(:accepted_message) { Fasteng::MessageSender::ReplyMessage::MESSAGES[:accept] }
+      let!(:user) { create(:user, telegram_id: from.id, status: 'registered') }
 
-      before do
-        allow(bot_api)
+      it 'user gets schedule' do
+        allow(Fasteng::MessageSender::ReplyMessage)
+          .to receive(:send)
+          .with(bot_api, chat.id, :accept)
+
+        expect { described_class.call(bot_api, message) }.not_to change(User, :count)
+        expect(User.find(user.id).status).to eq 'scheduled'
+      end
+
+      it 'user gets Accept message' do
+        expect(bot_api)
           .to receive(:send_message)
           .with(chat_id: chat.id, text: accepted_message)
 
-        @user = create(:user, telegram_id: from.id, status: 'registered')
-      end
-
-      it 'user gets schedule' do
-        expect { described_class.call(bot_api, message) }.not_to change(User, :count)
-
-        expect(User.find(@user.id).status).to eq 'scheduled'
+        described_class.call(bot_api, message)
       end
     end
 
     context 'when answer not valid' do
       let(:message) { double('message', text: 'what?', from: from, chat: chat) }
       let(:dialog_message) { Fasteng::MessageSender::ReplyMessage::MESSAGES[:welcome_dialog] }
+      let!(:user) { create(:user, telegram_id: from.id, status: 'registered') }
 
-      before do
-        allow(bot_api)
+      it 'user not get schedule' do
+        allow(Fasteng::MessageSender::ReplyMessage)
+          .to receive(:send)
+          .with(bot_api, chat.id, :welcome_dialog)
+
+        described_class.call(bot_api, message)
+
+        expect(User.find(user.id)).to have_attributes(
+          status: 'registered', schedule: nil, upcoming_time: nil
+        )
+      end
+
+      it 'user gets :welcome_dialog message' do
+        expect(bot_api)
           .to receive(:send_message)
           .with(chat_id: chat.id, text: dialog_message)
 
-        @user = create(:user, telegram_id: from.id, status: 'registered')
-      end
-
-      it 'user not get schedule' do
         described_class.call(bot_api, message)
-
-        expect(User.find(@user.id)).to have_attributes(
-          status: 'registered',
-          schedule: nil,
-          upcoming_time: nil
-        )
       end
     end
   end
 
-  describe 'User receive definition' do
+  describe 'User receives definition' do
     context 'when user make feedback' do
       let(:message) { double('message', text: '😄', from: from, chat: chat) }
       let(:confirm_message) { Fasteng::MessageSender::ReplyMessage::MESSAGES[:done] }
       let!(:user) { create(:user, telegram_id: from.id, status: 'waiting') }
 
-      before do
-        allow(bot_api)
-          .to receive(:send_message)
-          .with(chat_id: chat.id, text: confirm_message)
-      end
-
       it 'user status changed to scheduled' do
+        allow(Fasteng::MessageSender::ReplyMessage)
+          .to receive(:send)
+          .with(bot_api, chat.id, :done)
+
         described_class.call(bot_api, message)
 
         expect(User.find(user.id).status).to eq 'scheduled'
+      end
+
+      it 'user gets :done message' do
+        expect(bot_api)
+          .to receive(:send_message)
+          .with(chat_id: chat.id, text: confirm_message)
+
+        described_class.call(bot_api, message)
       end
     end
 
@@ -95,7 +109,7 @@ describe Fasteng::Dispatcher do
       let(:message) { double('message', text: 'wrong', from: from, chat: chat) }
       let!(:user) { create(:user, telegram_id: from.id, status: 'waiting') }
 
-      it 'user status left the same' do
+      it 'user status not changed' do
         described_class.call(bot_api, message)
 
         expect(User.find(user.id).status).to eq 'waiting'
