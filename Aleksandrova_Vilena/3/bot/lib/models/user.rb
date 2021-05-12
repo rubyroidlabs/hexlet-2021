f51a090875b0f4065efab34b9aa7b6079131f320
Aleksandrova Vilena <alviro12@gmail.com>
2021-05-12 09:38:47 +0500

    [task-3] robocop

--- Aleksandrova_Vilena/3/bot/lib/models/user.rb
+++ Aleksandrova_Vilena/3/bot/lib/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  enum status: [ :registered, :active, :pending, :stop ]
+  enum status: %i[registered active pending stop]
   has_many :learned_definitions, dependent: :destroy
   has_many :definitions, through: :learned_definitions
 
--- Aleksandrova_Vilena/3/bot/main.rb
+++
@@ -1,4 +0,0 @@
-require_relative 'config/environment'
-require 'pry'
-
-binding.pry
