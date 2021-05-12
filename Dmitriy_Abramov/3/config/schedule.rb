f7cfcec5c15ab43a278f4e0fdfd438f4768804fd
Dmitry Abramov <dmitry@Dmitrijs-MacBook-Pro.local>
2021-05-12 23:16:08 +0300

    fix imports

--- Dmitriy_Abramov/3/config/schedule.rb
+++ Dmitriy_Abramov/3/config/schedule.rb
@@ -5,13 +5,13 @@ env :PATH, ENV['PATH']
 set :job_template, nil
 
 # # every 30 minutes from 12 to 23
-# every '*/30 12-23 * * *' do
-#  rake 'app:send'
-#  rake 'app:remind'
-# end
-
-# every minute from 12 to 23
-every '*/1 12-23 * * *' do
+every '*/30 12-23 * * *' do
   rake 'app:send'
   rake 'app:remind'
 end
+
+# every minute from 12 to 23
+# every '*/1 12-23 * * *' do
+#   rake 'app:send'
+#   rake 'app:remind'
+# end
