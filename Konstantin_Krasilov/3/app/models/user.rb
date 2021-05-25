08da7079433b9b867c831f06b927791f41f0acd6
kkrasilov <kkrasilov@gmail.com>
2021-05-25 23:29:16 +0500

    Added aasm gem

--- Konstantin_Krasilov/3/app/models/user.rb
+++ Konstantin_Krasilov/3/app/models/user.rb
@@ -1,12 +1,30 @@
 # frozen_string_literal: true
+require 'aasm'
 
 class User < ActiveRecord::Base
-  enum conversation_status: {
-    send_max_word: 0,
-    send_smiley: 1,
-    conversation_break: 2
-  }
+  include AASM
 
   has_many :user_words, dependent: :destroy
   has_many :words, through: :user_words
+
+  aasm do
+    state :waiting_max_word, initial: true
+    state :sleeping, :waiting_smiley, :learning
+
+    event :answer_max_word do
+      transitions from: :sleeping, to: :waiting_max_word
+    end
+
+    event :answer_smiley do
+      transitions from: :learning, to: :waiting_smiley
+    end
+
+    event :learn do
+      transitions from: [:waiting_max_word, :waiting_smiley, :sleeping], to: :learning
+    end
+
+    event :sleep do
+      transitions from: :waiting_smiley, to: :sleeping
+    end
+  end
 end
