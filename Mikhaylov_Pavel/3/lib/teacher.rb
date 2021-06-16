742f3971f7be21ae439e5e02b2fbe229e51f6431
Pavel Mikhaylov <mikhailov.sztu@gmail.com>
2021-06-16 13:52:42 +0300

    logic for filtering users for sending and reminders has been moved from the model

--- Mikhaylov_Pavel/3/lib/teacher.rb
+++ Mikhaylov_Pavel/3/lib/teacher.rb
@@ -6,25 +6,23 @@ require_relative '../config/connection'
 require_relative 'postman/send_reminder'
 require_relative 'postman/send_word'
 require_relative 'services/new_word_for_user'
+require_relative 'services/users_needed_reminder_finder'
+require_relative 'services/users_needed_send_word_finder'
 
 class Teacher
   def self.send_word
-    User
-      .learning
-      .reject(&:done_for_today?)
-      .each do |user|
-        word = NewWordForUser.new(user).call
-        Postman::SendWord.send(word, user)
-        user.wait!
-      end
+    users_for_send = UsersNeededSendWordFinder.new(User.learning).call
+    users_for_send.each do |user|
+      word = NewWordForUser.new(user).call
+      Postman::SendWord.send(word, user)
+      user.wait!
+    end
   end
 
   def self.remind
-    User
-      .waiting
-      .filter(&:need_to_send_reminder?)
-      .each do |user|
-        Postman::SendReminder.send(user)
-      end
+    users_for_remind = UsersNeededReminderFinder.new(User.waiting).call
+    users_for_remind.each do |user|
+      Postman::SendReminder.send(user)
+    end
   end
 end
--- Mikhaylov_Pavel/3/models/user.rb
+++ Mikhaylov_Pavel/3/models/user.rb
@@ -4,12 +4,9 @@
 
 require 'aasm'
 require 'active_record'
-require_relative 'learned_word'
 
 class User < ActiveRecord::Base
   include AASM
-
-  REMINDER_IN_MINUTES = 90
 
   has_many :words, through: :learned_words
   has_many :learned_words, dependent: :destroy
@@ -31,15 +28,6 @@ class User < ActiveRecord::Base
     event :stop do
       transitions from: %i[unregistred learning waiting], to: :stopped
     end
-  end
-
-  def done_for_today?
-    LearnedWord.where(created_at: Time.current.all_day, user_id: id).count >= words_per_day
-  end
-
-  def need_to_send_reminder?
-    last_word_at = LearnedWord.where(user_id: id).last.created_at
-    (Time.current - last_word_at) / 60 > REMINDER_IN_MINUTES
   end
 end
 
